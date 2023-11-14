7b9729f099ddb2933a0e7b376d473155ea5225bd
Mikhail Aleinik <6424156@tut.by>
2023-11-14 17:22:07 +0300

    feat: added 6 tasks and tests

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -11,6 +11,7 @@
 
 /**
  * Returns a boolean value whether a number is positive. Relate zero to positive numbers.
+ * In this task, the use of methods of the Number and Math classes is not allowed.
  *
  * @param {number} number
  * @return {boolean}
@@ -25,24 +26,101 @@ function isPositive(/* number */) {
 }
 
 /**
- * Returns a boolean value indicating whether a number is an integer or not.
- * In the solution do not use methods of the Number class.
+ * Returns the maximum of three numbers.
+ * In this task, the use of methods of the Array class is not allowed.
  *
- * @param {number} number
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ *
+ * @example:
+ *  1, 2, 3       => 3
+ *  -5, 0, 5      => 5
+ *  -0.1, 0, 0.2  => 0.2
+ */
+function getMaxNumber(/* a, b, c */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns boolean whether the triangle is isosceles.
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
  * @return {boolean}
  *
  * @example:
- *  5  => true
- *  -5 => true
- *  0  => true
- *  5.5 => false
- *  0.5 => false
+ *  1, 2, 3   => false
+ *  3, 1, 2   => false
+ *  2, 3, 2   => true
+ *  3, 2, 2   => true
+ *  2, 2, 3   => true
+ *  2, 2, 5   => false
+ *  3, 0, 3   => false
  */
-function isInteger(/* number */) {
+function isIsoscelesTriangle(/* a, b, c */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns a boolean value whether a string is palindrome.
+ * In this task, the use of methods of the String and Array classes is not allowed.
+ *
+ * @param {string} str
+ * @return {boolean}
+ *
+ * @example:
+ *  'abcba'     => true
+ *  '0123210'   => true
+ *  'qweqwe'    => false
+ */
+function isPalindrome(/* str */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns the bool value whether the string contains the desired letter.
+ * In this task, the use of methods of the String and Array class is not allowed.
+ *
+ * @param {string} str
+ * @param {string} letter
+ * @return {boolean}
+ *
+ * @example:
+ *  'qwerty', 'q'     => true
+ *  'qwerty', 'Q'     => false
+ *  'qwerty', 'p'     => false
+ */
+function isContainLetter(/* str, letter */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns the bool value whether the number contains the desired digit.
+ * In this task, the use of methods of the String and Array class is not allowed.
+ *
+ * @param {number} num
+ * @param {number} digit
+ * @return {boolean}
+ *
+ * @example:
+ *  123450, 5   => true
+ *  123450, 1   => true
+ *  123450, 0   => true
+ *  12345, 0    => false
+ *  12345, 6    => false
+ */
+function isContainNumber(/* num, digit */) {
   throw new Error('Not implemented');
 }
 
 module.exports = {
   isPositive,
-  isInteger,
+  getMaxNumber,
+  isIsoscelesTriangle,
+  isPalindrome,
+  isContainLetter,
+  isContainNumber,
 };
--- test/conditions-n-loops-tests.js
+++ test/conditions-n-loops-tests.js
@@ -1,30 +1,210 @@
 const assert = require('assert');
 const tasks = require('../src/conditions-n-loops-tasks');
+const forbidden = require('../extensions/forbidden');
 it.optional = require('../extensions/it-optional');
 
 describe('core-js-conditions-n-loops', () => {
-  it.optional('isPositive should return a boolean value whether a number is positive', () => {
-    assert.equal(tasks.isPositive(10), true, '10 is a positive number');
-    assert.equal(tasks.isPositive(0), true, '0 is a positive number');
-    assert.equal(tasks.isPositive(-5), false, '-5 is a negative number');
-  });
-});
-
-describe('core-js-conditions-n-loops optimal implementation', () => {
-  it.optional('optimal implementation of isPositive', function test() {
-    const fnStr = tasks.isPositive.toString();
-    if (!fnStr.includes('return')) {
-      this.skip();
+  it.optional(
+    'isPositive should return a boolean value whether a number is positive',
+    () => {
+      assert.equal(tasks.isPositive(10), true, '10 is a positive number');
+      assert.equal(tasks.isPositive(0), true, '0 is a positive number');
+      assert.equal(tasks.isPositive(-5), false, '-5 is a negative number');
+      assert.equal(
+        forbidden.isCommented(tasks.isPositive),
+        false,
+        `Be sure to remove comments from the final solution`
+      );
     }
+  );
+  it.optional('getMaxNumber should return the maximum of three numbers', () => {
+    assert.equal(tasks.getMaxNumber(-5, 5, 1), 5);
+    assert.equal(tasks.getMaxNumber(5, 5, 5), 5);
+    assert.equal(tasks.getMaxNumber(0.5, 0.1, -5), 0.5);
+    assert.equal(tasks.getMaxNumber(3, -3, -1), 3);
     assert.equal(
-      fnStr.includes('else'),
+      forbidden.isCommented(tasks.getMaxNumber),
       false,
-      'redundant if/else construct used, simplify your solution',
+      `Be sure to remove comments from the final solution`
+    );
+    assert.equal(
+      forbidden.isArrayUsed(tasks.getMaxNumber),
+      false,
+      `Using methods of Array class is not allowed`
     );
   });
+  it.optional(
+    'isIsoscelesTriangle should return boolean whether the triangle is isosceles',
+    () => {
+      assert.equal(
+        tasks.isIsoscelesTriangle(1, 2, 3),
+        false,
+        '1, 2, 3 is non isosceles triangle'
+      );
+      assert.equal(
+        tasks.isIsoscelesTriangle(3, 1, 2),
+        false,
+        '3, 1, 2 is non isosceles triangle'
+      );
+      assert.equal(
+        tasks.isIsoscelesTriangle(2, 3, 2),
+        true,
+        '2, 3, 2 is isosceles triangle'
+      );
+      assert.equal(
+        tasks.isIsoscelesTriangle(3, 2, 2),
+        true,
+        '3, 2, 2 is isosceles triangle'
+      );
+      assert.equal(
+        tasks.isIsoscelesTriangle(2, 2, 3),
+        true,
+        '2, 2, 3 is isosceles triangle'
+      );
+      assert.equal(
+        tasks.isIsoscelesTriangle(2, 2, 5),
+        false,
+        '2, 2, 5 is non isosceles triangle'
+      );
+      assert.equal(
+        tasks.isIsoscelesTriangle(3, 0, 3),
+        false,
+        '3, 0, 3 is non isosceles triangle'
+      );
+      assert.equal(
+        forbidden.isCommented(tasks.isIsoscelesTriangle),
+        false,
+        `Be sure to remove comments from the final solution`
+      );
+    }
+  );
+  it.optional(
+    'isPalindrome should return a boolean value whether a string is palindrome',
+    () => {
+      assert.equal(
+        tasks.isPalindrome('abcba'),
+        true,
+        "'abcba' is a palindrome"
+      );
+      assert.equal(
+        tasks.isPalindrome('0123210'),
+        true,
+        "'0123210' is a palindrome"
+      );
+      assert.equal(
+        tasks.isPalindrome('qweqwe'),
+        false,
+        "'qweqwe' is not a palindrome"
+      );
+      assert.equal(
+        tasks.isPalindrome(' qweewq '),
+        true,
+        "' qweewq ' is a palindrome"
+      );
+      assert.equal(
+        tasks.isPalindrome('qweewq '),
+        false,
+        "'qweewq ' is not a palindrome"
+      );
+      assert.equal(
+        forbidden.isCommented(tasks.isPalindrome),
+        false,
+        `Be sure to remove comments from the final solution`
+      );
+      assert.equal(
+        forbidden.isStringUsed(tasks.isPalindrome),
+        false,
+        `Using methods of String class is not allowed`
+      );
+      assert.equal(
+        forbidden.isArrayUsed(tasks.isPalindrome),
+        false,
+        `Using methods of Array class is not allowed`
+      );
+    }
+  );
+  it.optional(
+    'isContainLetter should return a bool value if the string contains the desired letter',
+    () => {
+      assert.equal(
+        tasks.isContainLetter('qwerty', 'q'),
+        true,
+        "'qwerty' string contains a letter 'q'"
+      );
+      assert.equal(
+        tasks.isContainLetter('qwerty', 'Q'),
+        false,
+        "'qwerty' string not contains a letter 'Q'"
+      );
+      assert.equal(
+        tasks.isContainLetter('qwerty', 'P'),
+        false,
+        "'qwerty' string not contains a letter 'P'"
+      );
+      assert.equal(
+        forbidden.isCommented(tasks.isContainLetter),
+        false,
+        `Be sure to remove comments from the final solution`
+      );
+      assert.equal(
+        forbidden.isStringUsed(tasks.isContainLetter),
+        false,
+        `Using methods of String class is not allowed`
+      );
+      assert.equal(
+        forbidden.isArrayUsed(tasks.isContainLetter),
+        false,
+        `Using methods of Array class is not allowed`
+      );
+    }
+  );
+  it.optional(
+    'isContainNumber should bool value whether the number contains the desired digit',
+    () => {
+      assert.equal(
+        tasks.isContainNumber(123450, 5),
+        true,
+        '123450 number contains a digit 5'
+      );
+      assert.equal(
+        tasks.isContainNumber(123450, 1),
+        true,
+        '123450 number contains a digit 1'
+      );
+      assert.equal(
+        tasks.isContainNumber(123450, 0),
+        true,
+        '123450 number contains a digit 0'
+      );
+      assert.equal(
+        tasks.isContainNumber(12345, 0),
+        false,
+        '12345 number not contains a digit 0'
+      );
+      assert.equal(
+        tasks.isContainNumber(12345, 6),
+        false,
+        '12345 number not contains a digit 6'
+      );
+      assert.equal(
+        forbidden.isCommented(tasks.isContainNumber),
+        false,
+        `Be sure to remove comments from the final solution`
+      );
+      assert.equal(
+        forbidden.isStringUsed(tasks.isContainNumber),
+        false,
+        `Using methods of String class is not allowed`
+      );
+      assert.equal(
+        forbidden.isArrayUsed(tasks.isContainNumber),
+        false,
+        `Using methods of Array class is not allowed`
+      );
+    }
+  );
 });
 
-// describe('core-js-conditions-n-loops commented code check', () => {
-//   it.optional('commented code check', function test() {
-//   });
+// describe('core-js-conditions-n-loops optimal implementation', () => {
+//   it.optional('optimal implementation of isPalindrome', function test() {});
 // });
