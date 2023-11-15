9cd7937182a0bb9a974117ab73ac6a1ab1e096c1
Mikhail Aleinik <6424156@tut.by>
2023-11-15 17:58:31 +0300

    feat: added four tasks and tests

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -65,6 +65,24 @@ function isIsoscelesTriangle(/* a, b, c */) {
 }
 
 /**
+ * Returns the number converted to Roman numerals.
+ * The initial number will always be greater than zero and less than 40.
+ *
+ * @param {number} num
+ * @return {string}
+ *
+ * @example:
+ *  1   => I
+ *  2   => II
+ *  5   => V
+ *  10  => X
+ *  26  => XXVI
+ */
+function convertToRomanNumerals(/* number */) {
+  throw new Error('Not implemented');
+}
+
+/**
  * Returns a boolean value whether a string is palindrome.
  * In this task, the use of methods of the String and Array classes is not allowed.
  *
@@ -116,11 +134,69 @@ function isContainNumber(/* num, digit */) {
   throw new Error('Not implemented');
 }
 
+/**
+ * Returns a sorted array.
+ * If the direction is true it returns an ascending sorted array,
+ * if false it returns a descending sorted array
+ * In this task, the use of methods of the Array classes is not allowed.
+ *
+ * @param {Array<number>} arr
+ * @param {boolean} direction
+ * @return {boolean}
+ *
+ * @example:
+ *  [2, 9, 5], true     => [2, 5, 9]
+ *  [2, 9, 5], false    => [9, 5, 2]
+ */
+function sortByAsc(/* arr, direction */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns the original array rotated by an angle of 90 degrees clockwise.
+ * In this task, the use of methods of the String and Array class is not allowed.
+ *
+ * @param {Array<number>} arr
+ * @return {boolean}
+ *
+ * @example:
+ *  [                 [
+ *    [1, 2, 3],        [7, 4, 1],
+ *    [4, 5, 6],  =>    [8, 5, 2],
+ *    [7, 8, 9]         [9, 6, 3]
+ *  ]                 ]
+ */
+function rotateMatrix(/* arr */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns an index of element in the array of integers for which the sum
+ * of the elements to the left of it is equal to the sum of the elements on the right.
+ * If such an index does not return -1.
+ * In this task, the use of methods of the Array classes is not allowed.
+ *
+ * @param {number[]} arr
+ * @return {number}
+ *
+ * @example:
+ *  [1, 2, 5, 3, 0] => 2    => 1 + 2 === 3 + 0 then balance element is 5 and its index = 2
+ *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
+ *  [1, 2, 3, 4, 5] => -1   => no balancing element
+ */
+function getBalanceIndex(/* arr */) {
+  throw new Error('Not implemented');
+}
+
 module.exports = {
   isPositive,
   getMaxNumber,
   isIsoscelesTriangle,
+  convertToRomanNumerals,
   isPalindrome,
   isContainLetter,
   isContainNumber,
+  sortByAsc,
+  rotateMatrix,
+  getBalanceIndex,
 };
--- test/conditions-n-loops-tests.js
+++ test/conditions-n-loops-tests.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const tasks = require('../src/conditions-n-loops-tasks');
 const forbidden = require('../extensions/forbidden');
+const utility = require('../extensions/utility');
 it.optional = require('../extensions/it-optional');
 
 describe('core-js-conditions-n-loops', () => {
@@ -79,6 +80,27 @@ describe('core-js-conditions-n-loops', () => {
     }
   );
   it.optional(
+    'convertToRomanNumerals should return the number converted to Roman numerals',
+    () => {
+      assert.equal(tasks.convertToRomanNumerals(1), 'I');
+      assert.equal(tasks.convertToRomanNumerals(2), 'II');
+      assert.equal(tasks.convertToRomanNumerals(4), 'IV');
+      assert.equal(tasks.convertToRomanNumerals(5), 'V');
+      assert.equal(tasks.convertToRomanNumerals(8), 'VIII');
+      assert.equal(tasks.convertToRomanNumerals(10), 'X');
+      assert.equal(tasks.convertToRomanNumerals(13), 'XIII');
+      assert.equal(tasks.convertToRomanNumerals(19), 'XIX');
+      assert.equal(tasks.convertToRomanNumerals(21), 'XXI');
+      assert.equal(tasks.convertToRomanNumerals(29), 'XXIX');
+      assert.equal(tasks.convertToRomanNumerals(37), 'XXXVII');
+      assert.equal(
+        forbidden.isCommented(tasks.convertToRomanNumerals),
+        false,
+        `Be sure to remove comments from the final solution`
+      );
+    }
+  );
+  it.optional(
     'isPalindrome should return a boolean value whether a string is palindrome',
     () => {
       assert.equal(
@@ -124,35 +146,24 @@ describe('core-js-conditions-n-loops', () => {
     }
   );
   it.optional(
-    'isContainLetter should return a bool value if the string contains the desired letter',
+    'getIndexOf should return a bool value if the string contains the desired letter',
     () => {
+      assert.equal(tasks.getIndexOf('qwerty', 'q'), 0);
+      assert.equal(tasks.getIndexOf('qwerty', 't'), 4);
+      assert.equal(tasks.getIndexOf('qwerty', 'Q'), -1);
+      assert.equal(tasks.getIndexOf('qwerty', 'P'), -1);
       assert.equal(
-        tasks.isContainLetter('qwerty', 'q'),
-        true,
-        "'qwerty' string contains a letter 'q'"
-      );
-      assert.equal(
-        tasks.isContainLetter('qwerty', 'Q'),
-        false,
-        "'qwerty' string not contains a letter 'Q'"
-      );
-      assert.equal(
-        tasks.isContainLetter('qwerty', 'P'),
-        false,
-        "'qwerty' string not contains a letter 'P'"
-      );
-      assert.equal(
-        forbidden.isCommented(tasks.isContainLetter),
+        forbidden.isCommented(tasks.getIndexOf),
         false,
         `Be sure to remove comments from the final solution`
       );
       assert.equal(
-        forbidden.isStringUsed(tasks.isContainLetter),
+        forbidden.isStringUsed(tasks.getIndexOf),
         false,
         `Using methods of String class is not allowed`
       );
       assert.equal(
-        forbidden.isArrayUsed(tasks.isContainLetter),
+        forbidden.isArrayUsed(tasks.getIndexOf),
         false,
         `Using methods of Array class is not allowed`
       );
@@ -203,8 +214,106 @@ describe('core-js-conditions-n-loops', () => {
       );
     }
   );
+  it.optional('sortByAsc should return a sorted array', () => {
+    assert.deepEqual(tasks.sortByAsc([2, 9, 5]), [2, 5, 9]);
+    assert.deepEqual(tasks.sortByAsc([2, 9, 5, 9]), [2, 5, 9, 9]);
+    assert.deepEqual(tasks.sortByAsc([-2, 9, 5, -3]), [-3, -2, 5, 9]);
+    assert.deepEqual(
+      tasks.sortByAsc([-2, 9, 5, -3, 15, 0]),
+      [-3, -2, 0, 5, 9, 15]
+    );
+    assert.equal(
+      forbidden.isCommented(tasks.sortByAsc),
+      false,
+      `Be sure to remove comments from the final solution`
+    );
+    assert.equal(
+      forbidden.isArrayUsed(tasks.sortByAsc),
+      false,
+      `Using methods of Array class is not allowed`
+    );
+  });
+  it.optional(
+    'rotateMatrix should the original array rotated by an angle of 90 degrees clockwise',
+    () => {
+      let arr = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+      ];
+      let result = [
+        [7, 4, 1],
+        [8, 5, 2],
+        [9, 6, 3],
+      ];
+      assert.deepEqual(tasks.rotateMatrix(arr), result);
+
+      const min = -10;
+      const max = 10;
+      const matrixSize = 5;
+
+      arr = [];
+      for (let i = 0; i < matrixSize; i += 1) {
+        const line = utility.getRandomArray(min, max, matrixSize);
+        arr.push(line);
+      }
+      result = utility.getRotateMatrix(arr);
+      assert.deepEqual(tasks.rotateMatrix(arr), result);
+
+      assert.equal(
+        forbidden.isCommented(tasks.rotateMatrix),
+        false,
+        `Be sure to remove comments from the final solution`
+      );
+      assert.equal(
+        forbidden.isArrayUsed(tasks.rotateMatrix),
+        false,
+        `Using methods of Array class is not allowed`
+      );
+      assert.equal(
+        forbidden.isStringUsed(tasks.rotateMatrix),
+        false,
+        `Using methods of String class is not allowed`
+      );
+      assert.equal(
+        forbidden.isUtilityUsed(tasks.rotateMatrix),
+        false,
+        `Using functions on utility.js file is not allowed`
+      );
+    }
+  );
+  it.optional('getBalanceIndex should return a sorted array', () => {
+    assert.equal(tasks.getBalanceIndex([1, 2, 5, 3, 0]), 2);
+    assert.equal(tasks.getBalanceIndex([2, 3, 9, 5]), 2);
+    assert.equal(tasks.getBalanceIndex([1, 2, 3, 4, 5]), -1);
+    assert.equal(tasks.getBalanceIndex([1, 1]), -1);
+    assert.equal(tasks.getBalanceIndex([1, 2, 3, 4, 5, 6, 7, 8, 9]), -1);
+    assert.equal(tasks.getBalanceIndex([]), -1);
+    for (let i = 0; i < 5; i += 1) {
+      const balanced = utility.getBalancedArray(9);
+      assert.equal(tasks.getBalanceIndex(balanced.arr), balanced.balanceIndex);
+    }
+    assert.equal(
+      forbidden.isCommented(tasks.getBalanceIndex),
+      false,
+      `Be sure to remove comments from the final solution`
+    );
+    assert.equal(
+      forbidden.isArrayUsed(tasks.getBalanceIndex),
+      false,
+      `Using methods of Array class is not allowed`
+    );
+  });
 });
 
-// describe('core-js-conditions-n-loops optimal implementation', () => {
-//   it.optional('optimal implementation of isPalindrome', function test() {});
-// });
+describe('core-js-conditions-n-loops optimal implementation', () => {
+  it.optional('optimal implementation of sortByAsc', function test() {
+    const min = -1000;
+    const max = 1000;
+    const count = 10000;
+    const arr = utility.getRandomArray(min, max, count);
+    const sortedArr = arr.sort((a, b) => a - b);
+    this.timeout(30);
+    assert.deepEqual(tasks.sortByAsc(arr), sortedArr);
+  });
+});
