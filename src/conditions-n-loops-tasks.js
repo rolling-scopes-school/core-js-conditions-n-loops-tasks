ea5957ce83ec0c59f402bfb3da2edea70b885c6b
VasilyeuUV <vasilyeu.uv@mail.ru>
2024-01-09 09:30:30 +0300

    feat: solve getMaxNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,9 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  const max = a > b ? a : b;
+  return max > c ? max : c;
 }
 
 /**
